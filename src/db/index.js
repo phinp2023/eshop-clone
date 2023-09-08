216eb47419fc2441470c0a199c3e0ffbb2556498
phi_np <phi_np@dev.anybim.vn>
2023-09-08 14:30:10 +0700

    update

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDatabase = async () => {
     try {
         await mongoose
-            .connect(process.env.DB_URL, {
+            .connect(process.env.MONGODB_URI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             })
